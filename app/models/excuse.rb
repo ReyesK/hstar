326173429c3fd3897f8e73ff5df35443e79327ef
ReyesK <mr.kylereyes@gmail.com>
2015-03-13 12:02:27 -0400

    finished

--- app/models/excuse.rb
+++ app/models/excuse.rb
@@ -2,7 +2,7 @@ class Excuse < ActiveRecord::Base
 
 
   def self.info
-    "We have helped <b>#{Excuse.all.size}</b> people get out of things they don't want to do.".html_safe
+    "We have helped <span class='excuse_count'><b>#{Excuse.all.size}</b></span> people get out of things they don't want to do.".html_safe
   end
 
 end

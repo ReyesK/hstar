a61dae0a92692291d1e20e2851b71cc15b133a82
ReyesK <mr.kylereyes@gmail.com>
2015-03-13 12:01:17 -0400

    adds bold

--- app/models/excuse.rb
+++ app/models/excuse.rb
@@ -2,7 +2,7 @@ class Excuse < ActiveRecord::Base
 
 
   def self.info
-    "We have helped #{Excuse.all.size} people get out of things they don't want to do."
+    "We have helped <b>#{Excuse.all.size}</b> people get out of things they don't want to do.".html_safe
   end
 
 end

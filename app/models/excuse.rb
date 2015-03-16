f4a9cff6dfbffab3df7788b2f37b98996011845c
ReyesK <mr.kylereyes@gmail.com>
2015-03-16 08:07:16 -0400

    change way to get total excuses

--- app/models/excuse.rb
+++ app/models/excuse.rb
@@ -2,7 +2,7 @@ class Excuse < ActiveRecord::Base
 
 
   def self.info
-    "We have helped <span class='excuse_count'><b>#{Excuse.all.size}</b></span> people get out of things they don't want to do.".html_safe
+    "We have helped <span class='excuse_count'><b>#{Excuse.last.id}</b></span> people get out of things they don't want to do.".html_safe
   end
 
 end
